38b66f443afa9c00fc1340b07cbf5d755bc2a95d
vinh nguyen <nguyenvim010398@gmail.com>
2025-06-26 23:35:45 +0700

    them moi

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -14,9 +14,10 @@
             <button class="btn btn-sm btn-primary" @click="refreshList()">
                 <i class="fas fa-redo"></i> Làm mới
             </button>
-            <button class="btn btn-sm btn-success" @click="goToAddContact">
+            <router-link
+                :to="{ name: 'contact.add' }">
                 <i class="fas fa-plus"></i> Thêm mới
-            </button>
+        </router-link>
             <button class="btn btn-sm btn-danger" @click="removeAllContacts">
                 <i class="fas fa-trash"></i> Xóa tất cả
             </button>
@@ -29,6 +30,14 @@
             <i class="fas fa-address-card"></i>
             </h4>
             <ContactCard :contact="activeContact" />
+            <router-link
+                :to="{
+                name: 'contact.edit',
+                params: { id: activeContact._id },
+                }">
+        <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+        </router-link>
         </div>
     </div>
 </div>
